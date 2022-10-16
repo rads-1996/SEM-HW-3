82c4089b6a06a34f2ea6ae0537ae6045c9c1e1e7
Radhika Gupta <radsg.1996@gmail.com>
2022-10-15 20:45:44 -0700

    Changes made for Assignment-3

--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -1,15 +1,21 @@
 import Alamofire
+import Foundation
 
 public protocol WeatherService {
     func getTemperature() async throws -> Int
 }
 
-class WeatherServiceImpl: WeatherService {
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=corvallis&units=imperial&appid=<INSERT YOUR API KEY HERE>"
+enum baseUrl: String{
+    case realServer = "https://api.openweathermap.org/data/2.5/weather?lat=44.5646&lon=-123.2620&appid=b4863c4295d2382760f7a538b165ba84"
+}
 
+class WeatherServiceImpl: WeatherService {
+    
+    let url = baseUrl.realServer.rawValue
+    
     func getTemperature() async throws -> Int {
         return try await withCheckedThrowingContinuation { continuation in
-            AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
+            AF.request(url as URLConvertible, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
                 switch response.result {
                 case let .success(weather):
                     let temperature = weather.main.temp
@@ -24,10 +30,21 @@ class WeatherServiceImpl: WeatherService {
     }
 }
 
-private struct Weather: Decodable {
+//Given
+struct Weather: Decodable {
     let main: Main
 
     struct Main: Decodable {
         let temp: Double
     }
 }
+
+//Given
+let testData = """
+{
+    "main" :
+    {
+        "temp" : 56.7007697012432
+    }
+}
+""".data(using: .utf8)!
